2abf5ecc446cb13eec86932c93fc34d83e054393
erythana <ery@notavalidmail.com>
2020-08-29 20:36:07 +0200

    Registered command and conditions in palette

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,35 @@
 import * as vscode from 'vscode';
 import { LoggingModes } from './Models/loggingModes';
 import { ProjectTemplate } from './Models/ProjectTemplate';
-let prerequesitsMet = false;
+
 
 export function activate(context: vscode.ExtensionContext) {
 	// The code you place here will be executed once when the application loaded
 	let extensionName = context.extensionUri.path;
-	const dotnetTemplateString = 'dotnet2 new -l'; //TODO: Expose with settings
+	const dotnetTemplateString = 'dotnet new -l'; //TODO: Expose with settings
 
 	logToConsole(`Loaded extension ${extensionName}`, LoggingModes.information);
+	//Initially get all dotnet templates, if possible, and register the Reload Command which is only visible when the conditionsMet Context is not set to true
+	enumerateTemplatesHandler(dotnetTemplateString);
 
-	tryGetDOTNETTemplates(dotnetTemplateString).then(
-		(output) => {
-			//logToConsole(output as string, LoggingModes.information);
-			let templateArray = getTemplateArrFromDOTNETString(output as string);
-			logToConsole("Got all the available DOTNET Templates - now exposing them in File-Explorer context menu", LoggingModes.information);
-			prerequesitsMet = true;
-		}).catch((message) => {
-			logToConsole(message, LoggingModes.error);
-		});
+	//Register Commands
+	let enumerateCommand = vscode.commands.registerCommand('vscode-dotnet-menu.EnumerateTemplates', () => enumerateTemplatesHandler(dotnetTemplateString));
+	let menuCommand = vscode.commands.registerCommand('vscode-dotnet-menu.MenuCommand', () => {});//TODO: Only temporary, waiting for VSCode Devs to implement the menu item stuff at start of Oct. 2020
 
-	let disposable = vscode.commands.registerCommand('vscode-dotnet-menu.EnumerateTemplates', commandHandler);
-	
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(enumerateCommand, menuCommand);
 }
 
-function commandHandler(): any{ 
-	if (prerequesitsMet){
-		vscode.window.showInformationMessage('Activated Extension - remove after debugging');
-	}
-}
+const enumerateTemplatesHandler = (templateString: string) => 
+	{
+		tryGetDOTNETTemplates(templateString).then(
+			(output) => {
+				let templateArray = getTemplateArrFromDOTNETString(output as string);
+				logToConsole("Got all the available DOTNET Templates - now exposing them in File-Explorer context menu", LoggingModes.information);
+				vscode.commands.executeCommand('setContext', 'vscode-dotnet-menu:conditionsMet', true);
+			}).catch((message) => {
+				logToConsole(message, LoggingModes.error);
+			});	
+	};
 
 function getTemplateArrFromDOTNETString(dotnetOutput: string): ProjectTemplate[]
 {
@@ -57,7 +57,7 @@ function getTemplateArrFromDOTNETString(dotnetOutput: string): ProjectTemplate[]
 }
 
 function logToConsole(message: string, type: LoggingModes) {
-	console.log(`\n${new Date().toLocaleTimeString()} ${LoggingModes[type]}${message}`);
+	console.log(`---${new Date().toLocaleTimeString()} ${LoggingModes[type]}\n${message}`);
 }
 
 function tryGetDOTNETTemplates(availableDOTNETTemplates: string): Promise<unknown> {
@@ -65,7 +65,9 @@ function tryGetDOTNETTemplates(availableDOTNETTemplates: string): Promise<unknow
 		const cp = require('child_process');
 		cp.exec(availableDOTNETTemplates, (err: string, stdout: string, stderr: string) => {
 			if (stderr.length > 0) {//TODO: ADD FURTHER ERROR HANDLING
-				reject(`${stderr}Is the dotnet SDK installed on your system and available in your PATH variable?\nCheck if you get output from dotnet by typing 'dotnet --info' into command line`);
+				reject(`${stderr}Is the dotnet SDK installed on your system and available in your PATH variable?
+				\nCheck if you get output from dotnet by typing 'dotnet --info' into command line
+				\nAfter fixing stuff you might want to retry with the 'Reload DOTNET Templates' command`);
 			}
 			resolve(stdout);
 		});
